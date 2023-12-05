b76778568059f88da0e4b5792bc426918ef4e54a
ZoeAcero <zar.amazona@gmail.com>
2023-12-05 11:05:54 +0100

    9

--- EJ1/Estructura basica.cpp
+++ EJ1/Estructura basica.cpp
@@ -8,63 +8,14 @@ public:
     // Constructor
     Environment() {}
 
-    // agregamos simbolo
-    void addSymbol(const std::string& name, const std::any& value) {
-        symbols[name] = value;
-    }
-
-    //verificar si esta el simbolo
-bool hasSymbol(const std::string& name) const {
-        return symbols.find(name) != symbols.end();
-    }
-
-    //obtenemos el valor del simbolo
-    std::any getSymbol(const std::string& name) const {
-        auto it = symbols.find(name);
-        if (it != symbols.end()) {
-            return it->second;
-        } else {
-            // Devolver algo por defecto si el símbolo no está presente
-            return std::any();
-        }
-    }
-
-    //imprimir todos los simbolos
-    void printSymbols() const {
-        for (const auto& pair : symbols) {
-            std::cout << "Symbol: " << pair.first << ", Value: ";
-            try {
-                std::cout << std::any_cast<const char*>(pair.second) << std::endl;
-            } catch (const std::bad_any_cast&) {
-                std::cerr << "Type mismatch in symbol: " << pair.first << std::endl;
-            }
-        }
-    }
-
-    bool insert(const std::string& name, const std::any& value) {
+    void insert(const std::string& name, const std::variant<int, double, std::string>& value) {
         auto result = symbols.emplace(name, value);
-        return result.second;  // devuelve true si se agrego correctamente
+        if (!result.second) {
+            throw std::invalid_argument("Símbolo duplicado: " + name);
+        }
     }
 
-private:
-    std::map<std::string, std::any> symbols;
-};
 
-int main() {
-    Environment env;
-    env.addSymbol("x", 10);
-    env.addSymbol("message", std::string("Clase de programacion"));
-
-    bool success = env.insert("x", 100);
-
-    env.printSymbols();
-
-    std::cout << "x = " << std::any_cast<int>(env.getSymbol("x")) << std::endl;
-    std::cout << "y = " << std::any_cast<int>(env.getSymbol("y")) << std::endl;
-    std::cout << "z = " << std::any_cast<int>(env.getSymbol("z")) << std::endl;
-
-    return 0;
-}
 
 
 
