a1b2e5df95e77dfd081952dfb26e85aa12953dee
ZoeAcero <zar.amazona@gmail.com>
2023-12-05 10:45:59 +0100

    2

--- EJ1/Estructura basica.cpp
+++ EJ1/Estructura basica.cpp
@@ -2,3 +2,24 @@
 #include <string>
 #include <any>
 #include <iostream>
+
+class Environment {
+public:
+    // Constructor
+    Environment() {}
+
+    // agregamos simbolo
+    void addSymbol(const std::string& name, const std::any& value) {
+        int symbols;
+        symbols[name] = value;
+    }
+
+    //verificar si esta el simbolo
+bool hasSymbol(const std::string& name) const {
+        return symbols.find(name) != symbols.end();
+    }
+
+    //obtenemos el valor del simbolo
+    
+
+
