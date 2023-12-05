22bb51ae24c76391706c464ac3e355659be95bdf
ZoeAcero <zar.amazona@gmail.com>
2023-12-05 11:08:26 +0100

    9

--- EJ1/Estructura basica.cpp
+++ EJ1/Estructura basica.cpp
@@ -8,12 +8,26 @@ public:
     // Constructor
     Environment() {}
 
+    //insertar simbolo en el entorno
     void insert(const std::string& name, const std::variant<int, double, std::string>& value) {
         auto result = symbols.emplace(name, value);
         if (!result.second) {
             throw std::invalid_argument("Símbolo duplicado: " + name);
         }
     }
+
+    //buscar simbolo en el entorno
+    std::variant<int, double, std::string> find(const std::string& name) const {
+        auto it = symbols.find(name);
+        if (it == symbols.end()) {
+            throw std::invalid_argument("Símbolo no encontrado: " + name);
+        }
+        return it->second;
+    }
+    else {
+        throw std::invalid_argument("Símbolo no encontrado: " + name);
+    }
+}
 
 
 
